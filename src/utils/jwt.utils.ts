19ab017eb740ed7473fbdfa0d45be0f4c2521371
Richard Chou <konami99@hotmail.com>
2022-06-29 08:07:35 +1000

    remove console.log

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -5,14 +5,10 @@ export function signJwt (
   object: Object,
   options?: jwt.SignOptions | undefined
 ) {
-
-  console.log(`ACCESS_TOKEN_PRIVATE_KEY===${process.env.ACCESS_TOKEN_PRIVATE_KEY}`);
   const signingKey = Buffer.from(
     process.env.ACCESS_TOKEN_PRIVATE_KEY as string,
     "base64",
   ).toString("ascii");
-
-  console.log(`signingKey===${signingKey}`);
 
   return jwt.sign(object, signingKey, {
     ...(options && options),
@@ -24,13 +20,12 @@ export function verifyJwt(
   token: string,
 ) {
   const publicKey = Buffer.from(
-    process.env.ACCESS_TOKEN_PUBLIC_KEY as string,
+    process.env.ACCESS_TOKEN_PUBLIC_KEY as string, "base64").toString(
     "ascii",
   );
 
   try {
     const decoded = jwt.verify(token, publicKey);
-    console.log(`decoded------${decoded}`);
     return {
       valid: true,
       expired: false,
