4180dfe9c120925c455b7505bfc134e0d60cefa2
Richard Chou <richard@ezyraise.com>
2022-12-07 14:58:10 +1100

    validatePassword

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -4,10 +4,11 @@ import {
   createSession,
   findSessions,
 } from "../services/session.service";
+import { validatePassword } from "../services/user.service";
 import { signJwt } from "../utils/jwt.utils";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
-  const user = await UserModel.findOne({ email: req.body.email });
+  const user = await validatePassword(req.body);
 
   if (!user) {
     return res.status(401).send("Invalid email or password");
@@ -15,7 +16,7 @@ export async function createUserSessionHandler(req: Request, res: Response) {
 
   const session = await createSession(user._id, req.get("user-agent") || "");
   const accessToken = signJwt(
-    { ...user.toJSON(), session: session._id },
+    { ...user, session: session._id },
     { expiresIn: '15m' }
   )
 
